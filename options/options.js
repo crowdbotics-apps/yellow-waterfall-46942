2ffb9c3d08b5bc185886b99110e4826a16e74d9d
Crowdbotics <team@crowdbotics.com>
2024-03-11 22:28:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-waterfall-46942",
+  url: "https://yellow-waterfall-46942.botics.co",
+  api: "https://yellow-waterfall-46942.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
